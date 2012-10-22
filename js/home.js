a7ff46e612f76c05f38597316877ae2b01f373e8
Patomphong <patomphong.net@gmail.com>
2012-10-22 11:53:01 +0700

    Add_Prototype

---
+++ js/home.js
@@ -0,0 +1,62 @@
+//--------------------------------------------------------------------------- Tab Contract
+	function popitupAddContract(url) {
+		newwindow=window.open(url,'name','height=400,width=520,scrollbars=yes');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+	function popitupEditContract(url) {
+		newwindow=window.open(url,'name','height=400,width=520,scrollbars=yes');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+	function popitupPrintReport(url) {
+		newwindow=window.open(url,'name','height=200,width=620,scrollbars=yes');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+	function popitupPrintReportSummary(url) {
+		newwindow=window.open(url,'name','height=700,width=982');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+//--------------------------------------------------------------------------- Tab Service
+	function popitupAddService(url) {
+		newwindow=window.open(url,'name','height=500,width=700,scrollbars=yes');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+	function popitupEditService(url) {
+		newwindow=window.open(url,'name','height=500,width=700,scrollbars=yes');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+	function popitupPrintReportService(url) {
+		newwindow=window.open(url,'name','height=700,width=982');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+	function popitupHistory(url) {
+		newwindow=window.open(url,'name','height=200,width=670');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+//--------------------------------------------------------------------------- Tab Admin
+	function popitupAdduser(url) {
+		newwindow=window.open(url,'name','height=280,width=330');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+	
+	function popitupEdituser(url) {
+		newwindow=window.open(url,'name','height=270,width=330');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
